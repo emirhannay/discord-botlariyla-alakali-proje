f560f45edfad4ab0dff4a02cca3e99bd47301bf5
Emirhan AY <69421949+emirhannay@users.noreply.github.com>
2026-01-31 21:20:37 +0300

    Versiyon 1 oluşturuldu

---
+++ bot.py
@@ -0,0 +1,8 @@
+@bot.command()
+async def yardım(ctx):
+    await ctx.send("Komutlar:\n$hello \n$heh\n$temizle [miktar]\n$gecikme\n$yardım")
+
+@bot.command()
+async def randomemojigonderenfonksiyon(ctx):
+    #await ctx.send(randomemoji())
+    await ctx.send("yukarıdaki kod düzenlenmeli")
